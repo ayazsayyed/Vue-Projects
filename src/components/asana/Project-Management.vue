14fc9c52ad7f7b703b43a51c32fa1665572b63e1
Aayaz <ayaz.sayyed94@gmail.com>
2019-06-07 16:50:59 +0530

    Project Management Asana Initiated

---
+++ src/components/asana/Project-Management.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <navbar/>
+    <section class="pb-0 main-section" id="dashboard">
+      <div class="card shadow shadow-lg--hover"></div>
+      <main class="container-fluid mt-3">
+        <div class="row mb-4">
+          <div class="col-md-12">
+            <h3>Active Boards</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="boards-wrapper">
+              <div class="board card shadow shadow-lg--hover">
+                <div class="card-header">
+                  <div class="card-name"><h4> Card Name </h4></div>
+                  <div class="card-desc"><p> Card Description goes here </p></div>
+                </div>
+                <div class="card-footer">
+                  <div class="card-cta">
+                    <button class="btn  btn-sm btn-primary" type="button">View</button>
+                    <button class="btn  btn-sm btn-danger" type="button">Delete</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  </div>
+</template>
+
+
+
+<script>
+import navbar from "./navbar";
+export default {
+  name: "ProjectManagement",
+
+  components: {
+    navbar
+  },
+  data: function() {
+    return {
+      title: "Project - Management"
+    };
+  }
+};
+</script>
+
+<style scoped>
+#dashboard {
+  /* background: linear-gradient(87deg, #fb6340 0, #fbb140 100%) !important; */
+  height: 100vh;
+  text-align: left;
+}
+#dashboard .container-fluid{
+  height: 100%;
+}
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import memoryGame from './components/Memory-Game.vue'
 import home from './components/Landing.vue'
 import todo from './components/Todo.vue'
 import PageNotFound from './components/Page404.vue'
+import UserManagement from './components/user-management.vue'
+import Dashboard from './components/projects/dashboard.vue'
+import ProjectManagement from './components/asana/Project-Management.vue'
 
 
 Vue.config.productionTip = false
@@ -21,6 +24,9 @@ const routes = [
   { path: '/memory-game', component: memoryGame },
   { path: '/register', component: register },
   { path: '/user-list', component: userList},
+  { path: '/user-management', component: UserManagement},
+  { path: '/project-management', component: ProjectManagement},
+  { path: '/dashboard', component: Dashboard},
   { path: "*", component: PageNotFound},
 ];
 
